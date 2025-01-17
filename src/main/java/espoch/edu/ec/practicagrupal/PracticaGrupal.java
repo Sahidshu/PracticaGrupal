56542f705f46737139932ba3a4727d95139937ff
Lizbeth-girl <saezlizbethe6@gamail.com>
2025-01-17 16:23:09 -0500

    rama

--- src/main/java/espoch/edu/ec/practicagrupal/PracticaGrupal.java
+++ src/main/java/espoch/edu/ec/practicagrupal/PracticaGrupal.java
@@ -11,7 +11,6 @@ package espoch.edu.ec.practicagrupal;
 public class PracticaGrupal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("hthj");
+        
     }
 }
