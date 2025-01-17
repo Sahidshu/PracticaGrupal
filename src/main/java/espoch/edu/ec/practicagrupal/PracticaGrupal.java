ff82f20d127ec135df36e94b4ab608f6d82b15e9
Dilankl30 <lucerodk7@gmail.com>
2025-01-17 16:22:07 -0500

    RAma

--- src/main/java/espoch/edu/ec/practicagrupal/PracticaGrupal.java
+++ src/main/java/espoch/edu/ec/practicagrupal/PracticaGrupal.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
+
 package espoch.edu.ec.practicagrupal;
 
 /**
@@ -11,7 +12,10 @@ public class PracticaGrupal {
 
     public static void main(String[] args) {
 
-        Interfaz objInterfaz1 = new Interfaz();
+        
+        
+        Interfaz objInterfaz1= new Interfaz();
         objInterfaz1.setVisible(true);
+
     }
 }
