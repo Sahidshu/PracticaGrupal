e3f602332567190866ccc9654257469c804d79b4
Notteth <sahidmunoz0@gmail.com>
2025-01-17 15:36:36 -0500

    Practica

---
+++ src/main/java/espoch/edu/ec/practicagrupal/PracticaGrupal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.practicagrupal;
+
+/**
+ *
+ * @author sahid
+ */
+public class PracticaGrupal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
