61abe6906a536ee907eb5b8c64cbe34841673f3a
JIrleidySantos <jirleidysantosm@gmail.com>
2025-01-21 11:36:28 -0500

    Edicion

--- src/main/java/espoch/edu/ec/practicagrupal/PracticaGrupal.java
+++ src/main/java/espoch/edu/ec/practicagrupal/PracticaGrupal.java
@@ -11,11 +11,7 @@ package espoch.edu.ec.practicagrupal;
 public class PracticaGrupal {
 
     public static void main(String[] args) {
-
-        
-        
-        Interfaz objInterfaz1= new Interfaz();
-        objInterfaz1.setVisible(true);
+      
 
     }
 }
